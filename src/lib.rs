ad886d2fe98a867b1191643fc70986d85c120a8f
Ronald <rontbrilhante@Gmail.com>
2022-03-07 19:18:39 -0500

    basic server up and running now threaded. started to build own thread pool implementation. worker and job struct impl.

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,52 @@
+use std::sync::mpsc;
 use std::thread;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 
 }
 
 impl ThreadPool {
     /// Create a new ThreadPool.
-    ///
     /// The size is the number of threads in the pool.
-    ///
     /// #panics
-    ///
     /// The 'new' fn will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-        let mut threads = Vec::with_capacity(size);
-        for _ in 0..size {
-            // create threads
+
+        let (sender, receiver) = mpsc::channel();
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, receiver));
         }
-        ThreadPool { threads }
+        ThreadPool{workers,sender}
     }
 
     pub fn execute<F>(&self, f: F)
         where
             F: FnOnce() + Send + 'static
-    {}
+    {
+
+    }
+}
+
+/// worker exists so that we dont simply create a pool of threads
+/// and use then right way. instead we have the worker struct.
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+struct Job;
+
+impl Worker {
+    fn new(id:usize, receiver:mpsc::Receiver<Job>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+        Worker{id, thread}
+    }
 }
