9ed86e9878eaf1eaa8beff50d5217649cade9b73
Ronald <rontbrilhante@Gmail.com>
2022-03-07 19:33:48 -0500

    basic server up and running now threaded. started to build own thread pool implementation. worker and job struct impl.complete.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::sync::mpsc;
+use std::sync::{Arc, mpsc, Mutex};
 use std::thread;
 
 pub struct ThreadPool {
@@ -17,19 +17,23 @@ impl ThreadPool {
 
         let (sender, receiver) = mpsc::channel();
 
+        // Arc used for multithreading safety and Mutex for mutability, and lock on data
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id, receiver));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool{workers,sender}
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
         where
             F: FnOnce() + Send + 'static
     {
-
+        let job = Box::new(f);
+        self.sender.send(job).unwrap()
     }
 }
 
@@ -40,13 +44,21 @@ struct Worker {
     thread: thread::JoinHandle<()>,
 }
 
-struct Job;
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl Worker {
-    fn new(id:usize, receiver:mpsc::Receiver<Job>) -> Worker {
-        let thread = thread::spawn(|| {
-            receiver;
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver
+                .lock()
+                .unwrap()
+                .recv()
+                .unwrap();
+
+            println!("Worker {} got a job and is executing.", id);
+
+            job();
         });
-        Worker{id, thread}
+        Worker { id, thread }
     }
 }
